e17371bb8c96f76c733997d171d2e6da55fff8d6
Aditya <adityasinha95@gmail.com>
2021-06-01 01:31:24 +0530

    working for 1 url trying all +.+

--- main.py
+++ main.py
@@ -8,34 +8,53 @@ driver = webdriver.Chrome(PATH)
 # 
 # 
 #
+page_urls = list()
+total_links = 0
+clg_name = list()
+location = list()
 topic = list()
+last_date = list()
 
-def click_link_academicposition():
+def nav_academicposition_pages(pageurls):
+    url_count = 0
+    for url in pageurls:
+        driver.get(url)
+        clg_name.append((driver.find_element_by_xpath('//a[@class = "job-employer-title"][1]')).text)
+        topic.append((driver.find_element_by_xpath('//h1[@class = "job-title"][1]')).text)
+        location.append((driver.find_element_by_xpath('//a[@class = "job-location"][1]')).text)
+        last_date.append((driver.find_element_by_xpath('//span[@class = "job-publishing-date"][1]')).text)
+        if last_date[url_count] == '':
+            last_date[url_count] = 'DATE Not FOUND'
+        url_count += 1
+    print("clgname: ",clg_name)
+    print("topic: ",topic)
+    print("location: ",location)
+    print("last_date: ",last_date)
+    driver.close()
+
+
+def academicposition():
     time.sleep(3)
     try:
-        print("inside tryyy")
+        # finding the total number of links to lookup in the entire website
+        total_links = int(((driver.find_element_by_xpath('//span[@class="child-item-count"][1]')).text)[1:-1])
+
+        # collecting all the links
+        # navigating pages
         anch = driver.find_elements_by_tag_name("a")
         for i in  anch:
             if i.get_attribute("class") == "job__title js-gtm-joblink":
-                print("inside TRY ")
-                driver.get(i.get_attribute("href"))
-                # i.click()
-                print("found a page")
-                print("i = ", i)
-                print("title: ",driver.title)
-                # time.sleep(5)
-
-                # finding clgname
-                page_anch = driver.find_elements_by_tag_name("h1")
-                print("!!!!!!!!",type(page_anch))
-                print("QQQQQQQQQQQQ",len(page_anch))
-
-                for y in page_anch:
-                    print(y.text)
-                #     # print(y.get_attribute("class"))
-                #     if y.get_attribute("class") == "job-title":
-                #         print("CLASS FOUND")
-                #         print(y.text)
+                page_urls.append(i.get_attribute("href"))
+        if len(page_urls)<=total_links:
+            print(len(page_urls))
+            # press the next page button
+            path = driver.find_element_by_xpath('//a[@aria-label="Next"]')
+            print(path)
+            path.click()
+            academicposition()
+        else:
+            nav_academicposition_pages(page_urls)
+            driver.close()
     except:
         print("Dindint find any anch tag")
 # 
@@ -50,6 +69,6 @@ for iter in web_list:
     print(iter)
     driver.get(iter)
     if iter == "https://academicpositions.com/find-jobs/PhD-in-Chemistry-by-all-in-all/all/1":
-        click_link_academicposition()
+        academicposition()
 
 
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 import config as cfg
 import time
 
@@ -9,25 +10,36 @@ driver = webdriver.Chrome(PATH)
 # 
 #
 topic = list()
+clg_name = list()
+page_urls = list()
 
 def click_link_academicposition():
-    time.sleep(3)
-    anch = driver.find_elements_by_tag_name("h1")
-    # print("anch")
-    print(anch[0].text)
-                
+    print((driver.find_element_by_xpath('//a[@class = "job-employer-title"][1]')).text)
+    print((driver.find_element_by_xpath('//h1[@class = "job-title"][1]')).text)
+    print((driver.find_element_by_xpath('//a[@class = "job-location"][1]')).text)
+    if (driver.find_element_by_xpath('//span[@class = "job-publishing-date"][1]')):
+        print((driver.find_element_by_xpath('//span[@class = "job-publishing-date"][1]')).text)
+    else:
+        print("No Date Found")
 
+
+    driver.close()
+    
+    # driver.close()
 # 
 #
 # add website names here
 web_list = list()
-web_list = ["https://academicpositions.com/ad/max-planck-institute-for-solid-state-research/2021/phd-position-spintronics-with-van-der-waals-heterostructures/159117"]
- 
+# web_list = ["https://academicpositions.com/ad/max-planck-institute-for-solid-state-research/2021/phd-position-spintronics-with-van-der-waals-heterostructures/159117"]
+web_list = ["https://academicpositions.com/ad/national-institute-of-chemical-physics-and-biophysics-nicpb/2021/phd-position-synthesis-of-flexible-cavitands-as-efficient-drug-discovery-and-delivery-agents-and-sensors/158605"]
+
 for iter in web_list:
     print("inside for")
     print(iter)
     driver.get(iter)
-    if iter == "https://academicpositions.com/ad/max-planck-institute-for-solid-state-research/2021/phd-position-spintronics-with-van-der-waals-heterostructures/159117":
+    # if iter == "https://academicpositions.com/ad/max-planck-institute-for-solid-state-research/2021/phd-position-spintronics-with-van-der-waals-heterostructures/159117":
+    #     click_link_academicposition()
+    if iter == "https://academicpositions.com/ad/national-institute-of-chemical-physics-and-biophysics-nicpb/2021/phd-position-synthesis-of-flexible-cavitands-as-efficient-drug-discovery-and-delivery-agents-and-sensors/158605":
         click_link_academicposition()
 
 
