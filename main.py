dd5edcb8e139426ac18d9967f814be4c274f4042
Aditya <adityasinha95@gmail.com>
2021-06-02 00:00:51 +0530

    able to write data from website 1 in excel sheet

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from selenium import webdriver
 
 PATH = cfg.PATH + "/chromedriver"
 driver = webdriver.Chrome(PATH)
-# 
-# 
+#
+#
 #
 page_urls = list()
 total_links = 0
@@ -17,6 +17,7 @@ topic = list()
 last_date = list()
 bad_page_url = list()
 
+
 def nav_academicposition_pages(pageurls):
     url_count = 0
     for url in pageurls:
@@ -25,9 +26,8 @@ def nav_academicposition_pages(pageurls):
         try:
             clg_name.append((driver.find_element_by_xpath('//a[@class = "job-employer-title"][1]')).text)
             topic.append((driver.find_element_by_xpath('//h1[@class = "job-title"][1]')).text)
-            bad_page_url.append("")
+            bad_page_url.append("GOOD")
 
-        
             # if location class not fount at that path
             try:
                 try:
@@ -43,22 +43,32 @@ def nav_academicposition_pages(pageurls):
         except:
             clg_name.append("NO NAME FOUND")
             topic.append("NO TOPIC FOUND")
+            last_date.append("DATE NOT FOUND")
+            location.append("LOCATION NOT FOUND")
             bad_page_url.append(url)
         url_count += 1
-        print("We just finished taking values from url: ",url_count)
+        print("We just finished taking values from url: ", url_count)
+        print("clgname: ", len(clg_name))
+        print("topic: ", len(topic))
+        print("location: ", len(location))
+        print("last_date: ", len(last_date))
 
-    print("clgname: ",clg_name)
-    print("topic: ",topic)
-    print("location: ",location)
-    print("last_date: ",last_date)
-    data_index = pd.DataFrame({'COLLEGE NAME': clg_name,
-                               'TOPIC': topic,
-                               'LOCATION': location,
-                               'LAST DATE': last_date,
-                               'BAD PAGES': bad_page_url})
-    writer = pd.ExcelWriter('CHEM_PHD.xlsx', engine='xlsxwiter')
-    data_index.to_excel(writer, sheet_name='ACADEMIC_POSITION', index=False)
-    writer.save()
+    print("clgname: ", clg_name)
+    print("topic: ", topic)
+    print("location: ", location)
+    print("last_date: ", last_date)
+    
+    try:
+        df = pd.DataFrame({'COLLEGE NAME': clg_name,
+                            'TOPIC': topic,
+                            'LOCATION': location,
+                            'LAST DATE': last_date,
+                            'BAD PAGES': bad_page_url})
+        writer = pd.ExcelWriter('CHEM_PHD.xlsx', engine='xlsxwriter')
+        df.to_excel(writer, sheet_name='ACADEMIC_POSITION', index=False)
+        writer.save()
+    except:
+        print("FAILED To Create File")
     driver.close()
 
 
@@ -67,36 +77,46 @@ def academicposition():
     try:
         # finding the total number of links to lookup in the entire website
         total_links = int(((driver.find_element_by_xpath('//span[@class="child-item-count"][1]')).text)[1:-1])
+        print(total_links)
 
         # collecting all the links
         # navigating pages
-        anch = driver.find_elements_by_tag_name("a")
+        anch = driver.find_elements_by_xpath('//a[@class="job__title js-gtm-joblink"]')
+        print("Total links found", len(anch))
+        print("Total links displayed", total_links)
+        
+        # collecting all the links
         for i in  anch:
-            if i.get_attribute("class") == "job__title js-gtm-joblink":
-                page_urls.append(i.get_attribute("href"))
+            # if i.get_attribute("class") == "job__title js-gtm-joblink":
+            page_urls.append(i.get_attribute("href"))
+
+        # checking if we have got all the links
         if len(page_urls)<=total_links:
             print(len(page_urls))
+
             # press the next page button
             path = driver.find_element_by_xpath('//a[@aria-label="Next"]')
             print(path)
             path.click()
             academicposition()
         else:
-            nav_academicposition_pages(page_urls)
-            driver.close()
+            nav_academicposition_pages(page_urls[:5])
+            print("PRINT anything")
+            # driver.close()
     except:
-        print("Dindint find any anch tag")
+        print("Did not find any anch tag")
 # 
 # 
 #
 # add website names here
 web_list = list()
 web_list = ["https://academicpositions.com/find-jobs/PhD-in-Chemistry-by-all-in-all/all/1"]
+print(len(web_list))
 
-for iter in web_list:
-    print(iter)
-    driver.get(iter)
-    if iter == "https://academicpositions.com/find-jobs/PhD-in-Chemistry-by-all-in-all/all/1":
+for i in web_list:
+    print(i)
+    driver.get(i)
+    if i == "https://academicpositions.com/find-jobs/PhD-in-Chemistry-by-all-in-all/all/1":
         academicposition()
 
 
